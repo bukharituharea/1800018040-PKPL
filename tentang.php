12a587e7389c73f4ddf50a62526a1d607e73b510
bukharituharea <48221953+bukharituharea@users.noreply.github.com>
2022-01-24 12:15:05 +0700

    Add files via upload

---
+++ tentang.php
@@ -0,0 +1,51 @@
+<div class="box-title">
+    <p>Tentang / <b>Profil Pembuat</b></p>
+</div>
+<div id="box">
+
+  <h1>Tentang</h1>
+  <img src="img/p.png" width="150" align="left"/>
+  <table>
+    <tr>
+      <ul>
+        <td width="25%">
+          <li><b>NAMA</b></li>
+        </td>
+        <td>: Bukhari f.a Tuharea</td>
+      </ul>
+    </tr>
+
+    <tr>
+      <ul>
+        <td>
+          <li><b>NIM</b></li>
+        </td>
+        <td>: 1800018040</td>
+      </ul>
+    </tr>
+
+    <tr>
+      <ul>
+        <td>
+          <li><b>Jurusan</b></li>
+        </td>
+        <td>: S1 Teknik Informatika</td>
+      </ul>
+    </tr>
+
+    <tr>
+      <ul>
+        <td>
+          <li><b>Hobby</b></li>
+        </td>
+        <td>: Ngoding, Belajar bahasa pemrograman baru, Naik Gunung, dll</td>
+      </ul>
+    </tr>
+
+   
+
+   
+
+  </table>
+
+</div>
